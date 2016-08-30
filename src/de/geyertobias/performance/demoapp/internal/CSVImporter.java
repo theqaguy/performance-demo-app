22ff380bd598dd6b976099bad9118c8d9a5e7ef0
Tobias Geyer <tobias.geyer@gmx.net>
2016-08-30 19:22:55 +0200

    Added bigger data set Added debug messages to CSV Importer to trigger an OOME

--- src/de/geyertobias/performance/demoapp/internal/CSVImporter.java
+++ src/de/geyertobias/performance/demoapp/internal/CSVImporter.java
@@ -1,10 +1,10 @@
 package de.geyertobias.performance.demoapp.internal;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 import de.geyertobias.performance.demoapp.model.ModelProvider;
 
@@ -12,34 +12,50 @@ public class CSVImporter {
 
 	ArrayList<String> csvLines;
 	ArrayList<String[]> parsedLines;
+	ArrayList<String> debugOutput = new ArrayList<>();
 	
 	public void importFile(String selectedFile, ModelProvider modelProvider) {
+		addDebugOutput("Starting import of file "+selectedFile);
+		addDebugOutput("Setting up List for csv Lines");
 		csvLines = new ArrayList<>();
+		addDebugOutput("Setting up List for parsed Lines");
 		parsedLines = new ArrayList<>();
 		
+		addDebugOutput("Attempting to read file");
 		try (BufferedReader br = new BufferedReader(new FileReader(selectedFile))) {
 		    String line;
 		    while ((line = br.readLine()) != null) {
+		    	addDebugOutput("Read line |"+line+"| from file. Storing it for later parsing.");
 		    	// store line for later parsing
 		    	csvLines.add(line);
 		    }
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			addDebugOutput("Reading the file failed with an exception:");
+			addDebugOutput(e.getMessage());
+			addDebugOutput(e.getStackTrace().toString());
 		}
 		
+		addDebugOutput("Parsing stored lines");
+		
 		for(String line: csvLines) {
+			addDebugOutput("Parsing line |"+line+"|");
 			parsedLines.add(parseLine(line));
 		}
 		
+		addDebugOutput("Adding parsed lines to model");
+		int i = 0;
 		for(String[] parsedLine : parsedLines) {
+			addDebugOutput("Checking if line "+i+" was parsed correctly");
 			if(parsedLine.length==4) {
+				addDebugOutput("Adding line "+i+" to model - parameters: "+parsedLine[0]+", "+parsedLine[1]+", "+parsedLine[2]+", "+parsedLine[3]);
 				modelProvider.addAddress(parsedLine[0], parsedLine[1], parsedLine[2], Boolean.getBoolean(parsedLine[3]));
 			}
+			i++;
 		}
+	}
+
+	private void addDebugOutput(String string) {
+		debugOutput.add(new Date().toString()+ " - "+string);
 		
 	}
 
